bd27d47ed79b327fb7b4a8c3a7667246afb76031
jungai <juworaphol125@gmail.com>
2021-07-25 15:09:07 +0700

    use new custom store

--- src/state/lovelove.ts
+++ src/state/lovelove.ts
@@ -1,4 +1,4 @@
-import create from 'zustand';
+import { createStore } from '.';
 import { devtools } from 'zustand/middleware';
 
 export interface IUseLoveLoveState {
@@ -6,7 +6,7 @@ export interface IUseLoveLoveState {
 	changeMyLove: (person: string) => void;
 }
 
-export const useLoveLove = create<IUseLoveLoveState>(
+export const useLoveLove = createStore<IUseLoveLoveState>(
 	// dev tools can use with redux dev tools !!
 	// see https://github.com/pmndrs/zustand#redux-devtools
 	devtools((set) => ({
